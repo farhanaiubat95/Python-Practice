9f83147c9881645ed5623a1b9d22067a2360decb
Farhana Bente Islam <21303195@iubat.edu>
2024-07-27 12:40:37 +0600

    5th Commit- string

---
+++ String-document.py
@@ -0,0 +1,23 @@
+#string
+name='Farhana'
+name1=" Bente"
+name2=""" Islam"""
+print("My name is ",name,name1,name2)
+
+#using "escap sequence character \n , \t"
+str="I am 25 years old.\nI have a dream.\nI want to go abroad for higher education."
+print()
+print(str)
+
+#Concatenation
+str1="Py"
+str2="thon"
+total=str1+str2
+print()
+print(total)
+
+#length
+print("Total Length : ",len(total))
+print()
+total=str1+" "+str2
+print(total,".\nTotal Length : ",len(total))
