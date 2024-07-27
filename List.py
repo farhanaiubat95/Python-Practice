977406d1cb6e529741869389a48d029789169454
Farhana Bente Islam <21303195@iubat.edu>
2024-07-27 17:04:11 +0600

    8th commit- list

---
+++ List.py
@@ -0,0 +1,14 @@
+#list is Mutable(changable) / but string is immutable
+marks=[10,20,30,40,50] #marks[0], marks[1]
+print(marks)
+
+#different data type is accepted
+student=["Farhana",10,20.5,"Dhaka"]
+print()
+print(student)
+
+#list is mutable
+student[0]="Efah"
+student[3]=100000
+print()
+print("After changeing : ",student)
