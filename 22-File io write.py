e79fe1960452e953b0857d6b3c2a27b5ed46c665
Farhana Bente Islam <21303195@iubat.edu>
2024-07-31 00:49:04 +0600

    2nd commit- upadated problem solve

--- 22-File io write.py
+++ 22-File io write.py
@@ -4,13 +4,51 @@
 #w+ -  ||      ||                     - Truncate
 #a+ -  ||    append   (pointer end)   - No truncate
 
-f=open("sampleOfWrite.txt","a") #"w","a" - this two automatically create a file with given name
-data=f.write("This is appened 2...")
-print(data)
-f.close()
+# f=open("sampleOfWrite.txt","a") #"w","a" - this two automatically create a file with given name
+# data=f.write("This is appended 3...")
+# print(data)
+# f.close()
 
 
 #with - automatic close file
 with open("sampleOfWrite.txt","r") as f:
     data=f.read()
-    print(data)
+
+#replace file 
+new_data=data.replace("line","row")
+print(new_data)
+
+with open("sampleOfWrite.txt","w") as f: #overwrite on that file
+    f.write(new_data)
+    # f.write("\nThis is 2nd line")
+
+#use function
+
+# def check_for_word():
+#     word="Fake"
+#     with open("sampleOfWrite.txt","r") as f:
+#         data = f.read()
+#         if(data.find(word)!= -1):
+#             print("Found")
+#         else:
+#             print("Not Found")
+
+# check_for_word()
+
+
+#Line check
+def check_for_line():
+    word="Fake"
+    data=True
+    line_no=1
+    with open("sampleOfWrite.txt","r") as f:
+        while data:
+            data = f.readline()
+            if(word in data):
+                print(line_no)
+                return
+            line_no+=1
+    return -1
+
+print(check_for_line())
+
