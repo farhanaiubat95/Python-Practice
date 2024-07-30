bf721d7acf84d67d081d6398b168de4724d82f82
Farhana Bente Islam <21303195@iubat.edu>
2024-07-31 00:13:26 +0600

    1st commit- File i/o delete

---
+++ 23-File io delet.py
@@ -0,0 +1,6 @@
+#File i/o - delete
+#using the os module
+#Module (Like a code library) is a file written by another programmer that generally has
+#a functions we can use
+import os
+os.remove("file name.txt") #it will be deleted
