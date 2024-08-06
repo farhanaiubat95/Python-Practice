fe5f0459cd78f93b3640f9b5470479f70c3c5e68
Farhana Bente Islam <21303195@iubat.edu>
2024-08-06 16:02:47 +0600

    1st commit- Math import

---
+++ 34-Math.py
@@ -0,0 +1,24 @@
+#Math
+minimum=min(10,40,21)
+maximum=max(460,5,10)
+absolute =abs(-10.3)
+power=pow(10,2)
+print("Minimum Number : ",minimum)
+print("Maximum Number : ",maximum)
+print("Absolute Number : ",absolute)
+print("Power Number : ",power)
+print()
+
+
+#python has also a built-in module called math
+import math
+
+square = math.sqrt(16)
+ceil=math.ceil(2.5)
+floor=math.floor(2.5)
+pi=math.pi
+
+print("Square root : ",square)
+print("Ceiling : ",ceil)
+print("Floor : ",floor)
+print("Pi : ",pi)
