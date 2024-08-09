6f7ceecdbcec1245256e84b35919f0a4bd99c69f
Farhana Bente Islam <21303195@iubat.edu>
2024-08-09 20:15:13 +0600

    1st commit- virtual environment

--- 4-input.py
+++ 4-input.py
@@ -1,8 +1,8 @@
-#result for input() is always string
-name=input("Enter Name : ")
-age=int(input("Enter Your Age : "))
-price=float(input("Enter Price Details: "))
+# result for input() is always string
+name = input("Enter Name : ")
+age = int(input("Enter Your Age : "))
+price = float(input("Enter Price Details: "))
 
-print("My name is : ",name)
-print("My age is : ",age)
-print("My Book's price is : ",price)
+print("My name is : ", name)
+print("My age is : ", age)
+print("My Book's price is : ", price)
