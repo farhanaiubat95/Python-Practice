ea873cba51c55523109750ff72cda4506b5aa503
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2024-09-07 21:41:38 +0600

    update comments

--- Pillow folder/1.image edit/1.image.py
+++ Pillow folder/1.image edit/1.image.py
@@ -1,9 +1,10 @@
 from PIL import Image
 
 img = Image.open("Pillow folder/1.image edit/elon musk.jpg")
-img.show()
+img.show()  # image show
 img.rotate(45).show()  # rotate 45
 
+# image size print
 print(img.height)
 print(img.width)
 print(img.size)
