ce54d80a75d28f656b3f7995cd8ae2f3f0b6462c
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2024-09-07 22:58:47 +0600

    image croping

--- Pillow folder/1.image edit/1.image.py
+++ Pillow folder/1.image edit/1.image.py
@@ -2,7 +2,8 @@ from PIL import Image
 
 img = Image.open("Pillow folder/1.image edit/elon musk.jpg")
 img.show()  # image show
-img.rotate(45).show()  # rotate 45
+img_rotate = img.rotate(45)
+img_rotate.show()  # rotate 45
 
 # image size print
 print(img.height)
