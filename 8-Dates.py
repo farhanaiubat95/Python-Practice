5766abfbc188ac52dc2255c2e8646f59b6fa50da
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2024-09-14 14:46:32 +0600

    update dates.py

--- 8-Dates.py
+++ 8-Dates.py
@@ -1,25 +1,25 @@
-#Dates
+# Dates
 import datetime
 
-now_time=datetime.datetime.now()
-print("Time Now : ",now_time)
+now_time = datetime.datetime.now()
+print("Time Now : ", now_time)
 
-#separate time print
+# separate time print
 print()
-print("1. Year : ",now_time.year)
-print("2. Month : ",now_time.month)
-print("3. Date : ",now_time.day)
-print("4. Hour : ",now_time.hour)
-print("5. Minute : ",now_time.minute)
-print("6. Second : ",now_time.second)
-print("7. Microsecond : ",now_time.microsecond)
-print("8. Day : ",now_time.strftime("%U"))
+print("1. Year : ", now_time.year)
+print("2. Month : ", now_time.month)
+print("3. Date : ", now_time.day)
+print("4. Hour : ", now_time.hour)
+print("5. Minute : ", now_time.minute)
+print("6. Second : ", now_time.second)
+print("7. Microsecond : ", now_time.microsecond)
 
-#Data objects create - create a date: year, month, day.(hour, minute, second, microsecond, tzone)
+# The strftime() Method
+# The method is called strftime(), and takes one parameter, format, to specify the format of the returned string:
+print("8. Day : ", now_time.strftime("%U"))
+
+# Data objects create - create a date: year, month, day.(hour, minute, second, microsecond, tzone)
 print()
-x = datetime.datetime(2026, 5, 1, 10) #datetime() class requires three parameters
+# datetime() class requires three parameters
+x = datetime.datetime(2026, 5, 1, 10)
 print(x)
-
-
-#The strftime() Method
-#The method is called strftime(), and takes one parameter, format, to specify the format of the returned string:
